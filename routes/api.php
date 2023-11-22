fb046f7460b2c31ba08320949cfeffa240dd34c5
fitbouncer <bhuinjohn@gmail.com>
2023-11-23 01:07:16 +0530

    csrf

--- routes/api.php
+++ routes/api.php
@@ -22,15 +22,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/login', [AuthController::class, 'loginUser']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('/csrf-token', function () {
-    return response()->json([
-        'csrf_token' => csrf_token(),
-    ]);
-});
+
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/artist', [ArtistController::class, 'artistInfo']);
     Route::post('/artist-update/{id}', [ArtistController::class, 'artistUpdate']);
     Route::post('/upload-artwork', [ArtworkController::class, 'artworkUpload']);
+    Route::get('/csrf-token', function () {
+        return response()->json([
+            'csrf_token' => csrf_token(),
+        ]);
+    });
 });
 
 Route::get('/all-artworks', [ArtworkController::class, 'allArtwork'])->name('allArtwork');
