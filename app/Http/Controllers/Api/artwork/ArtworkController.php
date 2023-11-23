042212bd77e5f60c0939a3110812df122ebbe632
fitbouncer <bhuinjohn@gmail.com>
2023-11-23 21:03:07 +0530

    thero

--- app/Http/Controllers/Api/artwork/ArtworkController.php
+++ app/Http/Controllers/Api/artwork/ArtworkController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Api\artwork;
 
 use App\core\artwork\ArtworkInterface;
+use App\core\banner\BannerInterface;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ArtworkController extends Controller
 {
-    private $artworkInterface;
+    private $artworkInterface, $bannerInterface;
 
-    public function __construct(ArtworkInterface $artworkInterface)
+    public function __construct(ArtworkInterface $artworkInterface, BannerInterface $bannerInterface)
     {
         $this->artworkInterface = $artworkInterface;
     }
@@ -44,6 +45,26 @@ class ArtworkController extends Controller
         ]);
         $data = $request->only('user_id', 'style_id', 'placement_id', 'subject_id', 'image');
         $store = $this->artworkInterface->storeArtworkData($data);
+        if ($store) {
+            return response()->json([
+                'status' => true,
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'data' => 'Some error occur'
+            ]);
+        }
+    }
+
+    public function artworkBanner(Request $request) {
+        $request->validate([
+            'user_id' => 'required|numeric|exists:users,id',
+            'banner_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048'
+        ]);
+        $data = $request->only('user_id', 'banner_image');
+        $store = $this->bannerInterface->storeBannerImage($data);
         if ($store) {
             return response()->json([
                 'status' => true,
--- routes/api.php
+++ routes/api.php
@@ -23,15 +23,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'loginUser']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::group(['middleware' => 'auth:sanctum', 'custom.throttle'], function () {
     Route::get('/artist', [ArtistController::class, 'artistInfo']);
     Route::post('/artist-update/{id}', [ArtistController::class, 'artistUpdate']);
     Route::post('/upload-artwork', [ArtworkController::class, 'artworkUpload']);
-    Route::get('/csrf-token', function () {
-        return response()->json([
-            'csrf_token' => csrf_token(),
-        ]);
-    });
+    Route::post('/upload-banner', [ArtworkController::class, 'artworkBanner']);
+    // Route::get('/csrf-token', function () {
+    //     return response()->json([
+    //         'csrf_token' => csrf_token(),
+    //     ]);
+    // });
 });
 
 Route::get('/all-artworks', [ArtworkController::class, 'allArtwork'])->name('allArtwork');
